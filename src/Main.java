a9a9e66c5a8222dc5f23d9803a6f149cedc8a36e
CoolAleks <konalexhh@gmail.com>
2021-08-17 23:02:27 +0300

    miles

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price;  //необходим ввод значения пользователем
+        int bonus_rate=20;
+        int bonus_miles=price/bonus_rate;
+        System.out.println(bonus_miles);
+    }
+}
