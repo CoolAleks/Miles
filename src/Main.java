a6c6cd4e3318323d6751f26534a1915c8b7693cb
CoolAleks <87146838+CoolAleks@users.noreply.github.com>
2021-08-28 18:57:50 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
 
-        int price;  //необходим ввод значения пользователем
-        int bonusRate=20;
-        int bonusMiles=price/bonus_rate;
-        System.out.println(bonus_miles);
+        int price = 1000;  //необходим ввод значения пользователем
+        int bonusRate = 20;
+        int bonusMiles = price / bonusRate;
+        System.out.println(bonusMiles);
     }
 }
